771e674bd491d6947c3f78a310c35ef41adcec90
SEBonduel <sebastien050599@hotmail.fr>
2024-09-17 19:33:10 +0200

    project settings

---
+++ src/Components/PasswordGenerator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PasswordGenerator() {
+  return (
+    <div className='container'>
+      <h1>Password Generator</h1>
+    </div>
+  );
+}
+
+export default PasswordGenerator;
